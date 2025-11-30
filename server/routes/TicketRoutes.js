8ef0019036f3691af811e5edd36918b92857f7a0
gourav <gouravsharma20a@gmail.com>
2025-11-30 19:03:32 +0530

    making ui better

--- server/routes/TicketRoutes.js
+++ server/routes/TicketRoutes.js
@@ -1,12 +1,12 @@
-// routes/ticketRoutes.js
-const express = require("express");
-const router = express.Router();
-const { createTicket, getTicket, addUserMessage } = require("../controllers/TicketController");
+    // routes/ticketRoutes.js
+    const express = require("express");
+    const router = express.Router();
+    const { createTicket, getTicket, addUserMessage } = require("../controllers/TicketController");
 
 
-// public routes
-router.post("/", createTicket);
-router.get("/:id", getTicket);
-router.post("/:id/messages", addUserMessage);
+    // public routes
+    router.post("/", createTicket);
+    router.get("/:id", getTicket);
+    router.post("/:id/messages", addUserMessage);
 
-module.exports = router;
+    module.exports = router;
